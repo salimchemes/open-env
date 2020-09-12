57d73e2b0849c9ee4242c1cf47e640441f8b6043
Salim Chemes <salim@mobomo.com>
2020-09-12 20:10:28 -0300

    support url whitelist to dont validate

--- addEnv.js
+++ addEnv.js
@@ -1,3 +1,5 @@
+var whiteListUrls = ["localhost"];
+
 document.addEventListener(
   "DOMContentLoaded",
   function () {
@@ -35,15 +37,15 @@ function validURL(e) {
 
   var pattern = new RegExp(
     "^(https?:\\/\\/)?" + // protocol
-    "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
-    "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
-    "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
-    "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
       "(\\#[-a-z\\d_]*)?$",
     "i"
   ); // fragment locator
   const isValid = !!pattern.test(str);
-  if (!isValid) {
+  if (!isValid && !whiteListUrls.some((url) => str.includes(url))) {
     html = `<span id="invalidUrl" style="color: red">Not valid url</span>`;
   }
 
